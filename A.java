07985f0cae334299c853aee45c87050396c8369f
saitharun2000 <112924331+saitharun2000@users.noreply.github.com>
2023-03-29 21:50:52 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,15 @@
+interface A
+{
+void print()
+}
+class ab implements A
+{
+public void print()
+{
+System.out.println("hello i am suhash");
+}
+public static void main (String[]args)
+{
+ab o=new ab();
+o.print();
+}}
